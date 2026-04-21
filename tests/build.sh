5516435e1b13adfee2f144d9085f89f007cd2315
Neil Butcher <neil@Neils-MacBook-Pro.local>
2026-04-20 19:51:24 -0700

    added missing copyright and removed build.sh

--- tests/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hg++ --replacements pthread.h -DSST_HG_USE_MERCURY_PTHREAD -c test_tls.cc
-hg++ test_tls.o -o libtest_tls.so
-
-hg++ --replacements pthread.h -DSST_HG_USE_MERCURY_PTHREAD -c test_pthread.cc
-hg++ test_pthread.o -o libtest_pthread.so
